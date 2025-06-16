003271bc7dd00b3ea6faf73ff7d1ee7e910a8b4c
ycby <bryan_yc@yahoo.com>
2025-06-16 19:31:57 +0800

    Started with DatePicker Test

---
+++ test/helpers/date-picker/DatePicker.test.tsx
@@ -0,0 +1,26 @@
+import {render, screen, cleanup } from '@testing-library/react';
+import { describe, test, expect, afterEach } from 'vitest';
+import '@vitest/browser/context';
+import {DatePicker} from "#root/src/helpers/date-picker/DatePicker.tsx";
+import "@vitest/browser/context";
+import {dateToStringConverter} from "#root/src/helpers/DateHelpers.ts";
+
+describe('DatePicker', () => {
+
+    const testLabel = 'Test';
+
+    afterEach(cleanup);
+
+    test('Render the Date Picker with the right Label', async () => {
+
+        render(<DatePicker label={testLabel} value={new Date()} onChange={(date) => date } />);
+        expect(screen.getByText(testLabel)).toBeInTheDocument();
+    });
+
+    test('Check the Date matches the input Value', async () => {
+
+        render(<DatePicker label={testLabel} value={new Date()} onChange={(date) => date } />);
+        const todayAsString = dateToStringConverter(new Date());
+        await expect.element(screen.getByLabelText(testLabel)).toHaveValue(todayAsString);
+    });
+})
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    browser: {
+      provider: 'playwright',
+      enabled: true,
+      instances: [
+        {browser: 'firefox'},
+      ]
+    }
+  }
 })
