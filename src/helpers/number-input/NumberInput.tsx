164043fa8449baf1626f364d405ecbe76e7f4861
ycby <bryan_yc@yahoo.com>
2025-06-25 02:12:28 +0800

    added the formatted number input

---
+++ src/helpers/number-input/NumberInput.tsx
@@ -0,0 +1,77 @@
+import './NumberInput.css';
+
+type NumberInputProps = {
+    id?: string,
+    name: string,
+    value: string | null,
+    onChange: (value: string) => void,
+    type: string
+}
+
+const NumberInput = (props:NumberInputProps) => {
+
+    const {
+        id,
+        name,
+        value,
+        onChange,
+        type
+    } = props;
+
+    const valueString = value ? value.toString() : '';
+
+    let validationPattern: RegExp;
+    switch (type) {
+        case 'integer':
+            validationPattern = /^[0-9]*$/;
+            break;
+        case 'currency':
+            validationPattern = /^[0-9]*\.?[0-9]{0,2}$/;
+            break;
+        default:
+            validationPattern = /^[0-9]*$/;
+            break;
+    }
+
+    return (
+        <input
+            id={id}
+            className='number-input'
+            type='text'
+            name={name}
+            value={valueString}
+            onChange={(e) => {
+
+                const sanitizedValue = e.target.value.replace(/^[^0-9.]$/, '');
+
+                if (!validationPattern.test(sanitizedValue)) return;
+
+                onChange(sanitizedValue);
+            }}
+            onBlur={(e) => {
+
+                //TODO: consider cleaning up
+
+                if (type !== 'currency') return;
+
+                if (e.target.value === '') return;
+
+                if (/\.[0-9]{2}$/.test(e.target.value)) return;
+
+                //actual handler
+                if (e.target.value.indexOf('.') === -1) {
+
+                    onChange(e.target.value + '.00');
+                } else if (e.target.value.indexOf('.') === e.target.value.length - 1) {
+
+                    onChange(e.target.value + '00');
+                } else {
+
+                    onChange(e.target.value + '0');
+                }
+            }}
+        />
+    );
+}
+
+export default NumberInput;
--- src/routes/portfolio-diary/new-transaction-component/NewTransactionComponent.tsx
+++ src/routes/portfolio-diary/new-transaction-component/NewTransactionComponent.tsx
@@ -1,24 +1,29 @@
 import './NewTransactionComponent.css';
 import {useState} from "react";
+import NumberInput from "#root/src/helpers/number-input/NumberInput.tsx";
 
 const NewTransactionComponent = () => {
 
     const [transactionType, setTransactionType] = useState<string>('Buy');
-    const [amtWFee, setAmtWFee] = useState(0);
-    const [amtWOFee, setAmtWOFee] = useState(0);
-    const [quantity, setQuantity] = useState(0);
+    const [amtWFee, setAmtWFee] = useState('');
+    const [amtWOFee, setAmtWOFee] = useState('');
+    const [quantity, setQuantity] = useState('');
 
-    let preview = '';
+    let preview: string;
+
+    const amtWFeeNum = Number(amtWFee);
+    const amtWOFeeNum = Number(amtWOFee);
+    const quantityNum = Number(quantity);
 
     switch (transactionType) {
         case 'Buy':
-            preview = `${quantity ? quantity : 'QTY'} @ ${quantity && amtWOFee ? amtWOFee / quantity : 'PPS'} + ${amtWFee && amtWOFee ? amtWFee - amtWOFee : 'FEE'}`;
+            preview = `${quantityNum ? quantityNum : 'QTY'} @ ${quantityNum && amtWOFeeNum ? amtWOFeeNum / quantityNum : 'PPS'} + ${amtWFeeNum && amtWOFeeNum ? amtWFeeNum - amtWOFeeNum : 'FEE'}`;
             break;
         case 'Sell':
-            preview = `${quantity ? quantity : 'QTY'} @ ${quantity && amtWOFee ? amtWOFee / quantity : 'PPS'} - ${amtWFee && amtWOFee ? amtWFee - amtWOFee : 'FEE'}`;
+            preview = `${quantityNum ? quantityNum : 'QTY'} @ ${quantityNum && amtWOFeeNum ? amtWOFeeNum / quantityNum : 'PPS'} - ${amtWFeeNum && amtWOFeeNum ? amtWFeeNum - amtWOFeeNum : 'FEE'}`;
             break;
         case 'Dividend':
-            preview = `${quantity ? quantity : 'QTY'} @ ${quantity && amtWOFee ? amtWOFee / quantity : 'PPS'} + ${amtWFee && amtWOFee ? amtWFee - amtWOFee : 'FEE'}`;
+            preview = `${quantityNum ? quantityNum : 'QTY'} @ ${quantityNum && amtWOFeeNum ? amtWOFeeNum / quantityNum : 'PPS'} + ${amtWFeeNum && amtWOFeeNum ? amtWFeeNum - amtWOFeeNum : 'FEE'}`;
             break;
         default:
             preview = `An error occurred...`;
@@ -37,35 +42,35 @@ const NewTransactionComponent = () => {
                 </div>
                 <div className='new-transaction-component__item-container'>
                     <label>Amt w/ Fee</label>
-                    <input
-                        type='text'
-                        name='totalAmountWFee'
-                        value={amtWFee}
-                        onChange={(e) => {
-                        setAmtWFee(parseInt(e.target.value));
-                    }} />
+                    <div className='new-transaction-component__item-input'>
+                        <NumberInput
+                            type='currency'
+                            name='totalAmountWFee'
+                            value={amtWFee}
+                            onChange={setAmtWFee} />
+                    </div>
                 </div>
                 <div className='new-transaction-component__item-container'>
                     <label>Amt w/o Fee</label>
-                    <input
-                        type='text'
-                        name='totalAmountWOFee'
-                        value={amtWOFee}
-                        onChange={(e) => {
-                            setAmtWOFee(parseInt(e.target.value));
-                        }}
-                    />
+                    <div className='new-transaction-component__item-input'>
+                        <NumberInput
+                            type='currency'
+                            name='totalAmountWOFee'
+                            value={amtWOFee}
+                            onChange={setAmtWOFee}
+                        />
+                    </div>
                 </div>
                 <div className='new-transaction-component__item-container'>
                     <label>Quantity</label>
-                    <input
-                        type='text'
-                        name='quantity'
-                        value={quantity}
-                        onChange={(e) => {
-                            setQuantity(parseInt(e.target.value));
-                        }}
-                    />
+                    <div className='new-transaction-component__item-input'>
+                        <NumberInput
+                            name='quantity'
+                            value={quantity}
+                            onChange={setQuantity}
+                            type='integer'
+                        />
+                    </div>
                 </div>
             </div>
             <div className='new-transaction-component__preview'>
